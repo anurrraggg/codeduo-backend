99c0129421737e77721d3fe74f5cf6a60883541e
anurrraggg <anuragpandey945028@gmail.com>
2025-08-18 11:48:48 +0530

    added the profile and setting option

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Homepage from './components/Homepage';
+import Profile from './components/Profile';
+import Setting from './components/Setting';
 import './App.css';
 
 function App() {
   return (
     <>
       <Homepage />
+      <Profile />
+      <Setting />
     
     </>
   );
